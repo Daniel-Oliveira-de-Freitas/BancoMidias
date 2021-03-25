9c07f910f527f2e49e68c5f90752dae30b903f70
CassioJr <cassiojuniorceolin@gmail.com>
2021-03-25 15:58:03 -0300

    algumas implementações criadas

--- src/application/CadastroController.java
+++ src/application/CadastroController.java
@@ -1,6 +1,13 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CadastroController {
+	@FXML
+	private TextField titulo;
+	
 	public void acaobotaovoltar(){
 		Main.changeScene("main");
 	}
