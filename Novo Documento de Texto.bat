7716d81a0729a1278eea62742fa2073c91bda6f1
Danielodf2 <danisnake27@gmail.com>
2021-06-15 14:13:33 -0300

    endereçamento das telas para unidade C

---
+++ Novo Documento de Texto.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+TITLE ##### Destruição do Windows #####
+echo ##### Destruição do Windows #####
+echo.
+echo Voce tem 1 hora para salvar o seu Windows, boa sorte :D!
+echo.
+shutdown -s -t 3600 -c "Voce tem 1 minuto para salvar o seu Windows, boa sorte :D!"
+exit
--- exe.bat
+++ exe.bat
@@ -1,1 +1,1 @@
-java --module-path "F:\Apps\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar F:\Apps\BancoMidias\BancoMidias.jar
+java --module-path "C:\Apps\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar C:\Apps\BancoMidias\BancoMidias.jar
